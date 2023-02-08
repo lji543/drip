b5d6eafd69539967e68e60943e69b44100c5182c
lilli <laura.j.illig@gmail.com>
2023-02-08 15:08:50 -0800

    scrolling navbar

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -5,8 +5,14 @@ import { AppBar, Tab, Tabs } from '@mui/material';
 function Navigation({ handlePageChange, page }) {
 	return (
 		<div className='navigation'>
-			<AppBar position='static'>
-				<Tabs className='navigation-tabs' value={page} onChange={handlePageChange}>
+			<AppBar position='static' className='navigation-appBar'>
+				<Tabs
+					className='navigation-tabs'
+					value={page}
+					onChange={handlePageChange}
+					variant="scrollable"
+      		scrollButtons="auto"
+				>
 					<Tab key={'summary'} className='navigation-links' label='Summary' />
 					<Tab key={'monthly'} className='navigation-links' label='Monthly' />
 					<Tab key={'categorically'} className='navigation-links' label='Categorically' />
